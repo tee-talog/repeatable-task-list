f6ff9b8757c8b38cfa56f6b089716efbc002ff9d
talog <tee.talog@gmail.com>
2024-01-06 17:03:46 +0900

    show list view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,52 @@
-import Box from "@mui/material/Box"
-import Button from "@mui/material/Button"
+import { CheckBox } from "@mui/icons-material"
+import {
+  Box,
+  Button,
+  Checkbox,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material"
 import Container from "@mui/material/Container"
 
 export default function Home() {
   return (
     <Container maxWidth="xl">
       <Box>
-        <Button variant="contained">Hello world</Button>
+        <TableContainer>
+          <Table stickyHeader>
+            <TableHead>
+              <TableRow>
+                <TableCell>買うもの</TableCell>
+                <TableCell sx={{ width: "250px" }}>期限</TableCell>
+                <TableCell sx={{ width: "200px" }}>リピート</TableCell>
+                <TableCell sx={{ width: "200px" }}></TableCell>
+                <TableCell sx={{ width: "200px" }}></TableCell>
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              <TableRow hover>
+                <TableCell>牛乳</TableCell>
+                <TableCell>2024-01-01（5 日前）</TableCell>
+                <TableCell>
+                  <Checkbox inputProps={{ "aria-label": "リピート" }} />
+                </TableCell>
+                <TableCell>
+                  <Button variant="outlined">編集</Button>
+                </TableCell>
+                <TableCell>
+                  <Button variant="contained" color="success">
+                    買った
+                  </Button>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </Box>
     </Container>
   )

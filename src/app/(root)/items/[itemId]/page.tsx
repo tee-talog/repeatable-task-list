ba72706077638aabdab619e77eac5d5ddfdaf906
talog <tee.talog@gmail.com>
2024-01-06 22:43:45 +0900

    no dialog

--- src/app/(root)/items/[itemId]/page.tsx
+++ src/app/(root)/items/[itemId]/page.tsx
@@ -1,5 +1,5 @@
-import EditFormDialog from "@/components/EditFormDialog"
 import useItem from "@/hooks/useItem"
+import { Button } from "@mui/material"
 
 const ItemsId: React.FC<{ params: { itemId: string } }> = async ({
   params,
@@ -12,7 +12,14 @@ const ItemsId: React.FC<{ params: { itemId: string } }> = async ({
     console.log("test", formData)
   }
 
-  return <EditFormDialog item={item} onUpdate={update} />
+  return (
+    <form action={update}>
+      <h2>編集</h2>
+      <input name="name" type="text" value={item.name} />
+      <Button>キャンセル</Button>
+      <Button type="submit">OK</Button>
+    </form>
+  )
 }
 
 export default ItemsId
